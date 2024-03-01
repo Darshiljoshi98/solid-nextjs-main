9c9cc7535655f8b89dc3c6bfe84ffc491f4f1cb1
darshil.joshi <darshil.joshi@spec-india.com>
2024-03-01 09:52:58 +0530

    changes

--- components/Testimonial/SingleTestimonial.tsx
+++ components/Testimonial/SingleTestimonial.tsx
@@ -4,18 +4,49 @@ import '/'
 
 const SingleTestimonial = ({ review }: { review: Testimonial }) => {
   const { name, designation, image, content, pdflink } = review;
+  // const handleClick = async () => {   
+  //   // Download image.png from API
+  //   const apiResponse = await fetch('/api/file');
+  //   const imageBlob = await apiResponse.blob();
+  //   const imageUrl = window.URL.createObjectURL(imageBlob);
+  //   const imageLink = document.createElement('a');
+  //   imageLink.href = imageUrl;
+  //   imageLink.setAttribute('download', 'dummy.pdf');
+  //   document.body.appendChild(imageLink);
+  //   imageLink.click();
+  //   window.URL.revokeObjectURL(imageUrl);
+  // };
+
   const handleClick = async () => {   
-    // Download image.png from API
-    const apiResponse = await fetch('/api/file');
-    const imageBlob = await apiResponse.blob();
-    const imageUrl = window.URL.createObjectURL(imageBlob);
-    const imageLink = document.createElement('a');
-    imageLink.href = imageUrl;
-    imageLink.setAttribute('download', 'dummy.pdf');
-    document.body.appendChild(imageLink);
-    imageLink.click();
-    window.URL.revokeObjectURL(imageUrl);
+    try {
+    
+      const apiResponse = await fetch('/api/file');   
+  
+      if (!apiResponse.ok) {
+        throw new Error(`Failed to download file. Status: ${apiResponse.status}`);
+      }      
+
+      const fileBlob = await apiResponse.blob();     
+
+      const fileUrl = window.URL.createObjectURL(fileBlob);
+      const fileLink = document.createElement('a');
+      
+    
+      fileLink.href = fileUrl;
+      fileLink.setAttribute('download', 'downloaded-file.pdf'); 
+      
+
+      document.body.appendChild(fileLink);
+
+      fileLink.click();
+      
+  
+      window.URL.revokeObjectURL(fileUrl);
+    } catch (error) {
+      console.error('Error during file download:', error);
+    }
   };
+  
   return (
     <div className="rounded-lg bg-white p-9 pt-7.5 shadow-solid-9 dark:border dark:border-strokedark dark:bg-blacksection dark:shadow-none">
       <div className="mb-7.5 flex justify-between border-b border-stroke pb-6 dark:border-strokedark">
