8b1a7b7d98af1ce6fff0bedf19efe631acc5c94d
darshil.joshi <darshil.joshi@spec-india.com>
2024-11-26 16:37:53 +0530

    test

--- app/api/Testmonial/route.js
+++ app/api/Testmonial/route.js
@@ -2,6 +2,7 @@ import dbConn from "@/utils/dbConn";
 import Testimonial from "../../../models/testimonial";
 import { NextResponse } from "next/server";
 export async function GET(req, res) {
+    
     try {
         await dbConn();
         const ContactData = await Testimonial.find({}).sort({ createdAt: -1 });
